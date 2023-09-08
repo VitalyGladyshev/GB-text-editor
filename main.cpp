e5f3a37f49bed9bf3be61afa1ad7b3959c6c072d
Vitaly Gladyshev <48773035+VitalyGladyshev@users.noreply.github.com>
2023-09-08 22:47:54 +0300

    us4_t-001 Реализовал использование буфера обмена
    Реализовал редактирование текста с помощью буфера обмена и контекстное меню (копировать, вырезать, вставить)
    
    https://gb-project.yougile.com/team/dd43605206dc/#chat:9e8787a2e894

--- main.cpp
+++ main.cpp
@@ -11,10 +11,12 @@
 #include <QLocale>
 #include <QTranslator>
 #include <QScreen>
+#include <QCommandLineParser>
+//#include <QCommandLineOption>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QApplication appl(argc, argv);
     QCoreApplication::setApplicationName("Text Editor");
     QCoreApplication::setOrganizationName("GB command 2");
     QCoreApplication::setApplicationVersion(QT_VERSION_STR);
@@ -22,17 +24,30 @@ int main(int argc, char *argv[])
     // Средства интернационализации
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
-    for (const QString &locale : uiLanguages)
+    for (const QString &locale: uiLanguages)
     {
         const QString baseName = "TextEditor_" + QLocale(locale).name();
         if (translator.load(":/i18n/" + baseName))
         {
-            a.installTranslator(&translator);
+            appl.installTranslator(&translator);
             break;
         }
     }
 
+    // Инициализация парсера параметров командной строки
+    QCommandLineParser parser;
+    parser.setApplicationDescription("Text Editor");
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.addPositionalArgument("file", "The file to open.");
+    parser.process(appl);
+
     MainWindow textEditor;  // Создание экземпляра класса главного окна
+
+    // Открываем файлы, переданные в командной строке
+    const QStringList posArgs = parser.positionalArguments();
+    for (const QString &fileName : posArgs)
+        textEditor.OpenFile(fileName);
 
     // Задание оптимальных размеров окна
     const QRect availableGeometry = textEditor.screen()->availableGeometry();
@@ -42,5 +57,5 @@ int main(int argc, char *argv[])
             (availableGeometry.height() - textEditor.height()) / 2);
 
     textEditor.show();
-    return a.exec();
+    return appl.exec();
 }
