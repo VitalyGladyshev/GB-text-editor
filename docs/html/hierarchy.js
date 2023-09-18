4c922713a25f57f6cb22f32f606114dfd54fb9a4
Vitaly Gladyshev <48773035+VitalyGladyshev@users.noreply.github.com>
2023-09-18 22:22:40 +0300

    Обновление документации
    Обновление документации

---
+++ docs/html/hierarchy.js
@@ -0,0 +1,19 @@
+var hierarchy =
+[
+    [ "QDialog", null, [
+      [ "FindDialog", "class_find_dialog.html", null ],
+      [ "HyperlinkDialog", "class_hyperlink_dialog.html", null ]
+    ] ],
+    [ "QMainWindow", null, [
+      [ "MainWindow", "class_main_window.html", null ]
+    ] ],
+    [ "QObject", null, [
+      [ "Settings", "class_settings.html", null ]
+    ] ],
+    [ "QTextBrowser", null, [
+      [ "DocumentWindow", "class_document_window.html", null ]
+    ] ],
+    [ "QTreeView", null, [
+      [ "FileManager", "class_file_manager.html", null ]
+    ] ]
+];
